4227bf54f4f4d3256c3c9b6c094d2a2e8ccd6f74
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-10-23 08:01:40 +0100

    created birthday variable, imported Countdown, birthday to the component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import Countdown from './components/Countdown'
 
 function App() {
   const birthday = new Date('Dec 1,2023 09:00').getTime()
   console.log(birthday);
   return (
    <div>
-    <span>{birthday}</span>
+    <Countdown birthday={birthday} />
    </div>
   );
 }
