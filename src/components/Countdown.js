13e0fa357710a080b84a4f6a8dca603dbe4fc4f7
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-10-24 07:27:52 +0100

    Code fixed to show hours correcty

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -8,33 +8,38 @@ const Countdown = ({birthday}) => {
     const[isBirthday,setIsBirthday] = useState(false)
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            const today = new Date().getTime()
-            const timeLeft = birthday - today
-            console.log(timeLeft);
-
-            let day = 24*60*60*1000
-            let hour = 60*60*1000
-            let minute = 60*1000
-            let second = 1000
-
-            if(timeLeft > 0) {
-              let days = Math.floor(timeLeft / day)
-              let hours = Math.floor(timeLeft / hour)
-              let minutes = Math.floor(timeLeft % minute)
-              let seconds = Math.floor(timeLeft % second)
-
-              setDays(days)
-              setHours(hours)
-              setMinutes(minutes)
-              setSeconds(seconds)
-            } else {
-              setIsBirthday(true)
-            }
-        },1000)
+       
+      const timer = setInterval(() => {
+        const today = new Date().getTime()
+        const timeLeft = birthday - today
+        console.log(timeLeft);
+  
+        let day = 24*60*60*1000
+        let hour = 60*60*1000
+        let minute = 60*1000
+        let second = 1000
+       if(timeLeft > 0) {
+        let days = Math.floor(timeLeft / day)
+        let hours = Math.floor((timeLeft % day) / hour)
+        let minutes = Math.floor((timeLeft % hour) / minute)
+        let seconds = Math.floor((timeLeft % minute) / second) 
+  
+        setDays(days)
+        setHours(hours)
+        setMinutes(minutes)
+        setSeconds(seconds)
+       } else{
+          setIsBirthday(true)
+       }
+      },1000)
     },[birthday])
   return (
-    <div>Countdown</div>
+    <div>
+      <h1>{days}</h1>
+      <h1>{hours}</h1>
+      <h1>{minutes}</h1>
+      <h1>{seconds}</h1>
+    </div>
   )
 }
 
